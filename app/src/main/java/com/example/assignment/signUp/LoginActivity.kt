97a024e01f6aa3173388c15a8965e7ade68caacd
fzees <mohdzeeshansmartitventures@gmail.com>
2024-02-11 02:02:52 +0530

    ui

---
+++ app/src/main/java/com/example/assignment/signUp/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.assignment.signUp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.assignment.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/assignment/signUp/SignUPActivity.kt
@@ -0,0 +1,12 @@
+package com.example.assignment.signUp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.assignment.R
+
+class SignUPActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_upactivity)
+    }
+}
